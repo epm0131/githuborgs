9ffe75e01ff4f7b8e3c9a0600657e2052d900ba6
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-01 15:56:35 -0400

    I added my token code button and set up the click function in js

--- js/data.js
+++ js/data.js
@@ -2,6 +2,29 @@
   'use strict';
   window.fee = window.fee || {};
 
+  var myToken;
+
+
+  $('.tButton').on('click', function submitToken(event){
+      myToken = $('input').val();
+      $.ajax({
+        url: 'https://api.github.com/users/jisaacks/orgs',
+        method: 'GET',
+        dataType: 'json',
+        headers: {
+          Authorization: 'token ' + myToken
+        }
+      })
+      .done(function handleSuccess(data) {
+        console.log('in the success handler!');
+        console.log(data);
+      })
+      .fail(function errorCallback(xhr, errorType) {
+        console.log(xhr);
+      });
+
+  });
+
 
 
 
